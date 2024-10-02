2fa38734201240239ae8ee19b573b676768229e9
Murilo Ângelo <murapb07@gmail.com>
2024-10-02 10:28:37 -0300

    upd Player
    - Movement mechanic
    - Movement, running and rolling animations

---
+++ TopDownGame/Assets/Scripts/Player.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float runSpeed;
+
+    private Rigidbody2D rb;
+
+    private float initialSpeed;
+    private bool _isRunning;
+    private bool _isRolling;
+    private Vector2 _direction;
+
+    // Propriedade para acessar o valor do direction em outro script
+    public Vector2 direction
+    {
+        get { return _direction; }
+        set { _direction = value; }
+    }
+
+    public bool isRunning
+    {
+        get { return _isRunning; }
+        set { _isRunning = value; }
+    }
+
+    public bool isRolling
+    {
+        get { return _isRolling; }
+        set { _isRolling = value; }
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        initialSpeed = speed;
+    }
+
+    private void Update()
+    {
+        OnInput();
+        OnRun();
+        OnRolling();
+    }
+
+    private void FixedUpdate()
+    {
+        OnMove();
+    }
+
+    #region Movement
+
+    void OnInput()
+    {
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void OnMove()
+    {
+        rb.MovePosition(rb.position + _direction * speed * Time.fixedDeltaTime);
+    }
+
+    void OnRun()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+            _isRunning = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = initialSpeed;
+            isRunning = false;
+        }
+    }
+
+    void OnRolling()
+    {
+        // 0: Botao esquerdo do mouse
+        // 1: Botao direito do mouse
+        if (Input.GetMouseButtonDown(1))
+        {
+            speed = runSpeed;
+            _isRolling = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            speed = initialSpeed;
+            _isRolling = false;
+        }
+    }
+    #endregion
+}
