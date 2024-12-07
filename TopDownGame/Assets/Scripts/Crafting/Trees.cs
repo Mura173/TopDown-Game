340c55ddb3736a94e8a4c6298f44e99b81110646
Murilo Ângelo <murapb07@gmail.com>
2024-12-06 23:31:25 -0300

    upd Trees

--- TopDownGame/Assets/Scripts/Crafting/Trees.cs
+++ TopDownGame/Assets/Scripts/Crafting/Trees.cs
@@ -10,9 +10,14 @@ public class Trees : MonoBehaviour
     [SerializeField] private GameObject woodPrefab;
     [SerializeField] private int totalDropWood;
 
+    [SerializeField] private ParticleSystem leafs;
+
+    private bool isCut;
+
     public void OnHit()
     {
         treeHealth--;
+        leafs.Play();
 
         anim.SetTrigger("isHit");
 
@@ -24,12 +29,14 @@ public class Trees : MonoBehaviour
                 Instantiate(woodPrefab, transform.position + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f), transform.rotation);
             }           
             anim.SetTrigger("cut");
+
+            isCut = true;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Axe"))
+        if (other.CompareTag("Axe") && !isCut)
         {
             OnHit();
         }
